079c77c3186ee1d51ec3f46035ffc2ce2023efa6
haojunsng <snghaojun18@gmail.com>
2025-06-22 17:19:38 +0800

    [feat] Provision Lambda, Eventbridge, and more

---
+++ iac/variables.tf
@@ -0,0 +1,14 @@
+variable "bot_token" {
+  type = string
+  description = "The token for the bot"
+}
+
+variable "chat_id" {
+  type = string
+  description = "The chat ID for the bot"
+}
+
+variable "thread_id" {
+  type = string
+  description = "The thread ID for the bot"
+}
