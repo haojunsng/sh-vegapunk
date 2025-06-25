b47623b3585dabd01b6e3734253be7b3490936a0
haojunsng <snghaojun18@gmail.com>
2025-06-26 00:08:37 +0800

    feat: Use cloudflare worker as reverse proxy

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -110,3 +110,40 @@ resource "aws_iam_role_policy" "apigw_cloudwatch_policy" {
     }]
   })
 }
+
+resource "aws_apigatewayv2_api_policy" "weaponsLeft_api_policy" {
+  api_id = aws_apigatewayv2_api.weaponsLeft_api.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "execute-api:Invoke",
+        Resource = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.weaponsLeft_api.id}/*/*",
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = [
+              "173.245.48.0/20",
+              "103.21.244.0/22",
+              "103.22.200.0/22",
+              "103.31.4.0/22",
+              "141.101.64.0/18",
+              "108.162.192.0/18",
+              "190.93.240.0/20",
+              "188.114.96.0/20",
+              "197.234.240.0/22",
+              "198.41.128.0/17",
+              "162.158.0.0/15",
+              "104.16.0.0/13",
+              "104.24.0.0/14",
+              "172.64.0.0/13",
+              "131.0.72.0/22"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
---
+++ iac/data.tf
@@ -0,0 +1,3 @@
+# Get current account ID
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
