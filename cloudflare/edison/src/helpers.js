217ccf8b8de80b2302984c3010204e2fa768fbdf
haojunsng <snghaojun18@gmail.com>
2025-07-16 22:46:18 +0800

    feat: Add Openmeteo as 3rd weather source

--- cloudflare/edison/src/helpers.js
+++ cloudflare/edison/src/helpers.js
@@ -1,3 +1,21 @@
+const weatherCodeMap = {
+	0: "Clear sky",
+	1: "Mainly clear",
+	2: "Partly cloudy",
+	3: "Overcast",
+	45: "Fog",
+	51: "Light drizzle",
+	53: "Moderate drizzle",
+	55: "Dense drizzle",
+	61: "Slight rain",
+	63: "Moderate rain",
+	65: "Heavy rain",
+	80: "Slight rain showers",
+	81: "Moderate rain showers",
+	82: "Violent rain showers",
+	95: "Thunderstorm"
+}
+
 export async function sendTelegramMessage(c, chatId, text) {
 	const telegramUrl = `https://api.telegram.org/bot${c.env.BOT_TOKEN}/sendMessage`
 	try {
@@ -24,6 +42,7 @@ export async function sendTelegramMessage(c, chatId, text) {
 export function formatWeatherMessage(data) {
 	const tomorrow = data.tomorrow_io
 	const google = data.google
+	const openmeteo = data.openmeteo
 	const town = data.town
 
 	const capitalised_town = town.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
@@ -44,6 +63,11 @@ ${current_datetime}
 • Humidity: ${google.humidity}%
 • Precipitation: ${google.precipitationProbability}%
 • Temperature: ${google.temperature}°C
+
+🌦 OpenMeteo Weather:
+• ${weatherCodeMap[openmeteo.weathercode]}
+• Wind Speed: ${openmeteo.windspeed}%
+• Temperature: ${openmeteo.temperature}°C
 
 🤖 Powered by Edison 🤖
 `
